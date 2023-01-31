e68a2e8c88aaf7efd7d00105a89842968186d000
Zack_Mann <zackmann360@gmail.com>
2023-01-30 20:47:37 -0600

    save

--- week_9/youtube/script.js
+++ week_9/youtube/script.js
@@ -11,8 +11,11 @@ var loopTime = 12
 for(var i=0; i < loopTime; i += 1){
     // console.log(i)
     var clone = template.content.cloneNode(true)
+    var randomWidth = Math.round(100 + Math.random() * 200)
+    console.log(randomWidth)
+    clone.querySelector('.item img').src = "https://placekitten.com/" + randomWidth 
     var container = document.querySelector('.cloned')
-    console.log(container)
+    // console.log(container)
     container.append(clone)
 
 }
