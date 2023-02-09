0ca399e8682214968d2b9c02d006b0f9059aaa98
Zack_Mann <zackmann360@gmail.com>
2023-02-09 09:53:10 -0600

    save

---
+++ week_10/google_drive_nav_bar/javascript.js
@@ -0,0 +1,27 @@
+
+var menuButton = document.querySelector('.menuButton')
+menuButton.onclick = openMenu
+var dropdown = document.querySelector('.dropDown')
+dropdown.onclick = notAButton
+
+function openMenu(event){
+    // console.log(this)
+    // console.log(dropadown)
+    dropdown.classList.toggle('displayNone')
+    event.stopPropagation()
+}
+
+var body = document.querySelector('body')
+// console.log(body)
+body.onclick = close
+
+function close(){
+    console.log(this)
+    dropdown.classList.toggle('displayNone')
+}
+
+function notAButton(not){
+    console.log(dropdown)
+    not.stopPropagation()
+}
+
