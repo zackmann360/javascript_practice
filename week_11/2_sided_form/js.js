657f317541718e915888862463075cf13a7d2384
Zack_Mann <zackmann360@gmail.com>
2023-02-19 07:52:52 -0600

    save

---
+++ week_11/2_sided_form/js.js
@@ -0,0 +1,36 @@
+var app = document.querySelector('.app')
+// console.log(app)
+var inputgroupEmail = app.querySelector('#email')
+var inputEmail = inputgroupEmail.querySelector('input')
+// console.log(inputEmail)
+// console.log(inputgroupEmail)
+var inputgroupPass = app.querySelector('#pass')
+var inputPass = inputgroupPass.querySelector('input')
+// console.log(inputPass)
+// console.log(inputgroupPass)
+var button = app.querySelector('button')
+// console.log(button)
+button.onclick = onclickButton
+
+function onclickButton(){
+    // console.log(this)
+    if(!inputEmail.value){
+        inputgroupEmail.classList.add('error')
+    }
+    if(!inputPass.value){
+        inputgroupPass.classList.add('error')
+    }
+}
+
+inputEmail.onkeyup = onkeyupEmail
+
+function onkeyupEmail(){
+    console.log(this)
+    inputgroupEmail.classList.remove('error')
+}
+
+inputPass.onkeyup = onkeyupPass
+
+function onkeyupPass(){
+    inputgroupPass.classList.remove('error')
+}
