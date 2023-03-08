9607e20aeeae5fd09c27ee59ae926ccb961c1cb2
Zack_Mann <zackmann360@gmail.com>
2023-03-07 20:19:19 -0600

    save

--- week_14/login_form/js.js
+++ week_14/login_form/js.js
@@ -1,53 +1,51 @@
 var app = document.querySelector('.app')
+var groupEmail = app.querySelector('#groupEmail')
+var emailInput = groupEmail.querySelector('input')
+var groupPass = app.querySelector('#groupPass')
+var inputPass = groupEmail.querySelector('input')
 var button = app.querySelector('button')
 button.onclick = onclickButton
-var inputGroupEmail = app.querySelector('#email')
-var emailInput = inputGroupEmail.querySelector('input')
-var inputGroupPass = app.querySelector('#pass')
-var passInput = inputGroupPass.querySelector('input')
+
+groupEmail.onkeyup = onkeyupEmail
+groupPass.onkeyup = onkeyupPass
 
 
-// button.disabled = false
-// inputGroupEmail.classList.add('error')
-// inputGroupPass.classList.add('error')
+function onkeyupEmail(){
 
-inputGroupEmail.onkeyup = onkeyupInput
-inputGroupPass.onkeyup = onkeyupInput
-
-function onkeyupInput(){
-    button.disabled = true
-    if(emailInput.value && passInput.value){
-        button.disabled = false
+    console.log('email')
+    button.classList.add('enabled')
+    groupEmail.classList.add('error')
+    groupPass.classList.add('error')
+    if(groupEmail.value = true){
+        groupEmail.classList.remove('error')
     }
+    
+}
+
+function onkeyupPass(){
+    console.log('pass')
+    button.classList.add('enabled')
+    if(groupPass.value = true){
+        groupPass.classList.remove('error')
+    }
+
     
 }
 
 function onclickButton(){
-    
-    // add the error by default
-    inputGroupEmail.classList.add('error')
-    inputGroupPass.classList.add('error')
+    console.log(this)
+    if(emailInput.value != "admin" && inputPass.value != 'admin' ){
+        groupEmail.classList.add('error')
+        groupPass.classList.add('error')
+    }
 
-    // if login info is correct remove the error
-    if(emailInput.value === 'admin' && passInput.value === 'admin'){
-        inputGroupEmail.classList.remove('error')
-        inputGroupPass.classList.remove('error')
-        
-        
-        // remove show from the first app
-        app.classList.remove('show')
-        // add show to the next app
-        var successPage = document.querySelector('#successPage')
-        console.log(successPage)
-        successPage.classList.add('show')
+    if(emailInput.value === "admin" && inputPass.value === 'admin' ){
+        console.log('yes')
+        var current = document.querySelector('.show')
+        current.classList.remove('show')
+        var success = document.querySelector('.success')
+        success.classList.add('show')
     }
 
 
 }
-
-
-
-
-
-
-
