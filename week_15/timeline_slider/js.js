ed01601464d3620358105294d24caebfb386d09f
Zack_Mann <zackmann360@gmail.com>
2023-03-14 17:54:09 -0500

    save

--- week_15/timeline_slider/js.js
+++ week_15/timeline_slider/js.js
@@ -1,42 +1,36 @@
 var app = document.querySelector('.app')
+var actionGroups = app.querySelectorAll('.actionGroup')
 var highlight = app.querySelector('.highlight')
-var selected = app.querySelectorAll('.item')
-var dots = app.querySelectorAll('#dot')
-var width = 8
-var iconGroups = app.querySelectorAll('.iconGroup')
+var items = app.querySelectorAll('.item')
 
-for(var iconGroup of iconGroups){
-    iconGroup.onclick = onclickIconGroup
+
+for(var item of items){
+    item.onclick = onclickItem
 }
 
-function onclickIconGroup(){
-    var currentDot = app.querySelector('.selected')
-    currentDot.classList.remove('selected')
-    this.classList.add('selected')
-  
-}
-
-
-for(var select of selected){
-    select.onclick = onclickSelected
-}
-
-function onclickSelected(){
-    var attribute = this.getAttribute('num')
-
-    var position = attribute * width
-    highlight.style.left = position + 1.8 + 'rem' 
-
-    var pageAtt = this.getAttribute('pageTitle')
-
-    var currentPage = app.querySelector('.show')
-    
-    currentPage.classList.remove('show')
-
-    var selectedPage = app.querySelector('.' + pageAtt)
-    selectedPage.classList.add('show')
-
-    
+function onclickItem(){
+    var pageTitle = this.getAttribute('actionPage')
+    console.log(pageTitle)
+    var selectedPage = app.querySelector('.' + pageTitle)
+    console.log(selectedPage)
+    var currentPage = app.querySelector('.showTitle')
+    currentPage.classList.remove('showTitle')
+    selectedPage.classList.add('showTitle')
 }
 
 
+for(var actionGroup of actionGroups){
+    actionGroup.onclick = onclickActionGroup
+}
+
+var currentPosition = 12
+
+function onclickActionGroup(){
+    var num = this.getAttribute('num')
+    var newPossition = currentPosition * num
+    highlight.style.left = newPossition + "rem"
+    var currentHighlight = app.querySelector('.iconHighlight')
+    currentHighlight.classList.remove('iconHighlight')
+    this.classList.add('iconHighlight')
+
+}
