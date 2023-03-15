55098e8b40e0280de11491a89ec589712b8db7c1
Zack_Mann <zackmann360@gmail.com>
2023-03-14 20:54:03 -0500

    save

--- week_15/timeline_slider/js.js
+++ week_15/timeline_slider/js.js
@@ -1,36 +1,29 @@
 var app = document.querySelector('.app')
-var actionGroups = app.querySelectorAll('.actionGroup')
+var tabs = app.querySelectorAll('.actionGroup')
 var highlight = app.querySelector('.highlight')
-var items = app.querySelectorAll('.item')
 
-
-for(var item of items){
-    item.onclick = onclickItem
+for(var tab of tabs){
+    tab.onclick = onclickTab
 }
 
-function onclickItem(){
-    var pageTitle = this.getAttribute('actionPage')
-    console.log(pageTitle)
-    var selectedPage = app.querySelector('.' + pageTitle)
-    console.log(selectedPage)
-    var currentPage = app.querySelector('.showTitle')
-    currentPage.classList.remove('showTitle')
-    selectedPage.classList.add('showTitle')
-}
+var left = 12
+function onclickTab(){
+    var currentHighlight = app.querySelector('.active')
+    currentHighlight.classList.remove('active')
+    this.classList.add('active')
+    
+    var currentTitle = app.querySelector('.show')
+    currentTitle.classList.remove('show')
+    
+    var titleAtt = this.getAttribute('boxTitle')
+    var newTitle = app.querySelector('.' + titleAtt)
+    newTitle.classList.add('show')
+    console.log(newTitle)
+    console.log(titleAtt)
+    
+    var selectedNum = this.getAttribute('num')
+    highlight.style.left = selectedNum * left + 'rem'
 
 
-for(var actionGroup of actionGroups){
-    actionGroup.onclick = onclickActionGroup
-}
-
-var currentPosition = 12
-
-function onclickActionGroup(){
-    var num = this.getAttribute('num')
-    var newPossition = currentPosition * num
-    highlight.style.left = newPossition + "rem"
-    var currentHighlight = app.querySelector('.iconHighlight')
-    currentHighlight.classList.remove('iconHighlight')
-    this.classList.add('iconHighlight')
 
 }
